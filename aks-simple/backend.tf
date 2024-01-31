be3878e81677a20178c9a2baf50b0d01dfc46480
Luke Lamberg <32557712+lambergluke@users.noreply.github.com>
2024-01-31 14:03:47 -0600

    Update backend.tf

--- aks-simple/backend.tf
+++ aks-simple/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate"
-    storage_account_name = "terraformstatelessss"
+    storage_account_name = "luketfstate"
     container_name       = "statey"
   }
 }
